d9c6856eb224300c1fab139ce4c5cf84b8ca29d6
sarahnifsa <sarahtb25@gmail.com>
2018-08-06 20:24:40 +0800

    data validation add to cart

--- imports/templateformcarts.js
+++ imports/templateformcarts.js
@@ -7,3 +7,42 @@ import './templateformcarts.html';
 Template.templateformcarts.onCreated(function () {
   Meteor.subscribe('carts');
 });
+
+Template.templateformcarts.onRendered(function(){
+	$('.templateformcarts').validate({
+        rules: {
+            title: {
+                required: true
+            },
+            seller: {
+                required: true
+            }
+        },
+        messages: {
+            title: {
+                required: "You must enter the name of the product.",
+                title: "You've entered an invalid product name."
+            },
+            seller: {
+                required: "You must enter a password.",
+                seller: "You've entered an invalid seller."
+            }
+        }
+    });
+    var validatorformcarts = $('.templateformcarts').validate({
+        submitHandler: function(event){
+            var title = $('[name=title]').val();
+            var seller = $('[name=seller]').val();
+            Meteor.AddtoCart(title, seller, function(error){
+                if(error){
+                    validatorformcarts.showErrors({
+        			title: title.reason
+        			)};
+        			validatorformcarts.showErrors({
+        			seller: seller.reason
+        			)};       
+                } else {}
+            });
+        }
+    });
+});
