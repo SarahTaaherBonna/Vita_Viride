428521256730f72bff4ae4acd1854873cddfe818
sarahnifsa <sarahtb25@gmail.com>
2018-08-09 15:20:42 +0800

    did some form of data validation for carts and products collections

--- Collections/Products.js
+++ Collections/Products.js
@@ -8,36 +8,43 @@ Products.attachSchema(new SimpleSchema({
   seller: {
 		type: String,
 		label: "Seller",
+		optional: false,
 	},
 
   title: {
 		type: String,
 		label: "Title",
+		optional: false,
 	},
 
 	details: {
 		type: String,
 		label: "Details",
+		optional: false,
 	},
 
 	price: {
 		type: String,
 		label: "Price",
+		optional: false,
 	},
 
 	Halalapproved: {
 		type: String,
 		label: "Halal",
+		optional: false,
 	},
 
 	Veganapproved: {
 		type: String,
 		label: "Vegan",
+		optional: false,
 	},
 
 	Vegetarianapproved: {
 		type: String,
 		label: "Vegetarian",
+		optional: false,
 	},
 
 	tags: {
